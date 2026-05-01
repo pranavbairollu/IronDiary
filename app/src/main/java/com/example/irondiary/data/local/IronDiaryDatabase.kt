20a35e1d69cc62cdf2209e323166a2c51271d753
Pranav Bairollu <pranavbairollu@gmail.com>
2026-05-01 23:25:29 +0530

    Fix Room schema version mismatch

--- app/src/main/java/com/example/irondiary/data/local/IronDiaryDatabase.kt
+++ app/src/main/java/com/example/irondiary/data/local/IronDiaryDatabase.kt
@@ -18,7 +18,7 @@ import com.example.irondiary.data.local.entity.DailyLogEntity
         StudySessionEntity::class, 
         DailyLogEntity::class
     ], 
-    version = 2, 
+    version = 3, 
     exportSchema = false
 )
 @TypeConverters(RoomTypeConverters::class)
